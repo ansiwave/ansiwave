8c8dd771a295fbbf73ccc12f0044375d50dff0a9
oakes <zach@sekao.net>
2021-04-02 11:09:19 +0200

    Only return commands when parsing

--- src/ansiwavepkg/wavescript.nim
+++ src/ansiwavepkg/wavescript.nim
@@ -1,37 +1,31 @@
-import unicode
+import unicode, sequtils
 
 type
-  TokenKind* = enum
-    Newline, Whitespace, Command
-  Token* = object
+  Command = object
     text: string
-    kind*: TokenKind
     line, columnStart, columnEnd: int
 
 const
   slash = "/".runeAt(0)
   newline = "\n".runeAt(0)
 
-proc tokenize*(text: string): seq[Token] =
+proc parseCommands*(text: string): seq[Command] =
   var
     i = 0
     line = 0
     column = 0
     match: seq[Rune]
     command = false
-  proc flush(res: var seq[Token]) =
+  proc flush(res: var seq[Command]) =
     if match.len > 0:
       let columnStart = column
       column += match.len
       if match[0] == slash:
-        res.add(Token(text: $match, kind: Command, line: line, columnStart: columnStart, columnEnd: column))
-      else:
-        res.add(Token(text: $match, kind: Whitespace, line: line, columnStart: columnStart, columnEnd: column))
+        res.add(Command(text: $match, line: line, columnStart: columnStart, columnEnd: column))
       match = @[]
   for ch in runes(text):
     if ch == newline:
       flush(result)
-      result.add(Token(text: $newline, kind: Newline, line: line, columnStart: column, columnEnd: column + 1))
       line += 1
       column = 0
       command = false
--- tests/test1.nim
+++ tests/test1.nim
@@ -6,3 +6,10 @@ test "Dedupe codes":
   const text = "\e[31m\e[32m\e[41;42;43mHello, world!\e[31m"
   let newText = text.dedupeCodes
   check newText.escape == "\e[32;43mHello, world!\e[31m".escape
+
+import ansiwavepkg/wavescript
+
+test "Parse commands":
+  const text = staticRead("hello.ansiwave")
+  let cmds = text.parseCommands
+  check cmds.len == 2
