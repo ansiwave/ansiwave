24b4a58172c6a4e346c6955a097c4064a3da6dac
oakes <zach@sekao.net>
2021-04-02 10:31:04 +0200

    Use escape in test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,8 +1,8 @@
 import unittest
 import ansiwave
-import re
+import strutils
 
 test "Dedupe codes":
   const text = "\e[31m\e[32m\e[41;42;43mHello, world!\e[31m"
   let newText = text.dedupeCodes
-  check newText.replace(re"\e", "e") == "e[32;43mHello, world!e[31m"
+  check newText.escape == "\e[32;43mHello, world!\e[31m".escape
