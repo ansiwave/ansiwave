0f6480151e518a303833aa8520f8a9e45e758d2c
oakes <zach@sekao.net>
2021-09-24 04:46:48 +0200

    Fix paths sent via passL and passC

--- src/ansiwavepkg/chafa.nim
+++ src/ansiwavepkg/chafa.nim
@@ -1,5 +1,8 @@
 when defined(amd64) and (defined(macosx) or defined(linux)):
-  {.passC: "-Isrc/ansiwavepkg/chafa -Isrc/ansiwavepkg/chafa/internal".}
+  import os
+  const dir = currentSourcePath().parentDir().joinPath("chafa")
+
+  {.passC: "-I" & dir & " -I" & dir.joinPath("internal").}
   {.passC: "-mavx2".}
   {.compile: "chafa/chafa.c".}
   {.compile: "chafa/chafa-canvas-config.c".}
@@ -32,143 +35,143 @@ when defined(amd64) and (defined(macosx) or defined(linux)):
   {.compile: "chafa/internal/smolscale/smolscale-avx2.c".}
   {.compile: "chafa/internal/smolscale/smolscale.c".}
 
-  {.passC: "-Isrc/ansiwavepkg/chafa/glib -Isrc/ansiwavepkg/chafa/glib/gnulib".}
+  {.passC: "-I" & dir.joinPath("glib") & " -I" & dir.joinPath("glib/gnulib").}
   when defined(windows):
-    {.passC: "-Isrc/ansiwavepkg/chafa/glib-windows".}
+    {.passC: "-I" & dir.joinPath("glib-windows").}
     {.passL: "-luuid -lintl -lws2_32 -lole32"}
   elif defined(macosx):
-    {.passC: "-Isrc/ansiwavepkg/chafa/glib-macos".}
+    {.passC: "-I" & dir.joinPath("glib-macos").}
     {.passL: "-liconv -lintl -framework CoreFoundation -framework AppKit"}
   elif defined(linux):
-    {.passC: "-Isrc/ansiwavepkg/chafa/glib-linux".}
+    {.passC: "-I" & dir.joinPath("glib-linux").}
 
   {.passC: "-DGLIB_COMPILATION -DLIBDIR".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gmessages.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/garcbox.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/garray.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gasyncqueue.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gatomic.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gbacktrace.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gbase64.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gbitlock.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gbookmarkfile.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gbytes.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gcharset.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gchecksum.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gconvert.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gdataset.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gdate.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gdatetime.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gdir.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/genviron.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gerror.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gfileutils.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/ggettext.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/ghash.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/ghmac.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/ghook.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/ghostutils.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/giochannel.c".}
+  {.compile: "chafa/glib/gmessages.c".}
+  {.compile: "chafa/glib/garcbox.c".}
+  {.compile: "chafa/glib/garray.c".}
+  {.compile: "chafa/glib/gasyncqueue.c".}
+  #{.compile: "chafa/glib/gatomic.c".}
+  {.compile: "chafa/glib/gbacktrace.c".}
+  #{.compile: "chafa/glib/gbase64.c".}
+  {.compile: "chafa/glib/gbitlock.c".}
+  #{.compile: "chafa/glib/gbookmarkfile.c".}
+  {.compile: "chafa/glib/gbytes.c".}
+  {.compile: "chafa/glib/gcharset.c".}
+  #{.compile: "chafa/glib/gchecksum.c".}
+  {.compile: "chafa/glib/gconvert.c".}
+  #{.compile: "chafa/glib/gdataset.c".}
+  #{.compile: "chafa/glib/gdate.c".}
+  #{.compile: "chafa/glib/gdatetime.c".}
+  {.compile: "chafa/glib/gdir.c".}
+  {.compile: "chafa/glib/genviron.c".}
+  {.compile: "chafa/glib/gerror.c".}
+  {.compile: "chafa/glib/gfileutils.c".}
+  {.compile: "chafa/glib/ggettext.c".}
+  {.compile: "chafa/glib/ghash.c".}
+  #{.compile: "chafa/glib/ghmac.c".}
+  #{.compile: "chafa/glib/ghook.c".}
+  {.compile: "chafa/glib/ghostutils.c".}
+  {.compile: "chafa/glib/giochannel.c".}
   when defined(windows):
-    {.compile: "src/ansiwavepkg/chafa/glib/giowin32.c".}
+    {.compile: "chafa/glib/giowin32.c".}
   else:
-    {.compile: "src/ansiwavepkg/chafa/glib/giounix.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gkeyfile.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/glib-init.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/glib-private.c".}
+    {.compile: "chafa/glib/giounix.c".}
+  #{.compile: "chafa/glib/gkeyfile.c".}
+  {.compile: "chafa/glib/glib-init.c".}
+  #{.compile: "chafa/glib/glib-private.c".}
   when not defined(windows):
-    {.compile: "src/ansiwavepkg/chafa/glib/glib-unix.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/glist.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gmain.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gmappedfile.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gmarkup.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gmem.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gnode.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/goption.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gpattern.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gpoll.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gprimes.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gprintf.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gqsort.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gquark.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gqueue.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/grand.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/grcbox.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/grefcount.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/grefstring.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gregex.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gscanner.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gsequence.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gshell.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gslice.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gslist.c".}
+    {.compile: "chafa/glib/glib-unix.c".}
+  {.compile: "chafa/glib/glist.c".}
+  {.compile: "chafa/glib/gmain.c".}
+  #{.compile: "chafa/glib/gmappedfile.c".}
+  #{.compile: "chafa/glib/gmarkup.c".}
+  {.compile: "chafa/glib/gmem.c".}
+  #{.compile: "chafa/glib/gnode.c".}
+  {.compile: "chafa/glib/goption.c".}
+  {.compile: "chafa/glib/gpattern.c".}
+  {.compile: "chafa/glib/gpoll.c".}
+  #{.compile: "chafa/glib/gprimes.c".}
+  {.compile: "chafa/glib/gprintf.c".}
+  {.compile: "chafa/glib/gqsort.c".}
+  {.compile: "chafa/glib/gquark.c".}
+  {.compile: "chafa/glib/gqueue.c".}
+  {.compile: "chafa/glib/grand.c".}
+  {.compile: "chafa/glib/grcbox.c".}
+  {.compile: "chafa/glib/grefcount.c".}
+  #{.compile: "chafa/glib/grefstring.c".}
+  #{.compile: "chafa/glib/gregex.c".}
+  #{.compile: "chafa/glib/gscanner.c".}
+  #{.compile: "chafa/glib/gsequence.c".}
+  {.compile: "chafa/glib/gshell.c".}
+  {.compile: "chafa/glib/gslice.c".}
+  {.compile: "chafa/glib/gslist.c".}
   when defined(windows):
-    {.compile: "src/ansiwavepkg/chafa/glib/gspawn-win32-helper.c".}
-    {.compile: "src/ansiwavepkg/chafa/glib/gspawn-win32.c".}
+    {.compile: "chafa/glib/gspawn-win32-helper.c".}
+    {.compile: "chafa/glib/gspawn-win32.c".}
   else:
-    {.compile: "src/ansiwavepkg/chafa/glib/gspawn.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gstdio-private.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gstdio.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gstrfuncs.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gstring.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gstringchunk.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gstrvbuilder.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gtester.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gtestutils.c".}
+    {.compile: "chafa/glib/gspawn.c".}
+  #{.compile: "chafa/glib/gstdio-private.c".}
+  {.compile: "chafa/glib/gstdio.c".}
+  {.compile: "chafa/glib/gstrfuncs.c".}
+  {.compile: "chafa/glib/gstring.c".}
+  #{.compile: "chafa/glib/gstringchunk.c".}
+  #{.compile: "chafa/glib/gstrvbuilder.c".}
+  #{.compile: "chafa/glib/gtester.c".}
+  {.compile: "chafa/glib/gtestutils.c".}
   when defined(windows):
-    {.compile: "src/ansiwavepkg/chafa/glib/gthread-win32.c".}
+    {.compile: "chafa/glib/gthread-win32.c".}
   else:
-    {.compile: "src/ansiwavepkg/chafa/glib/gthread-posix.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gthread.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gthreadpool.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gtimer.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gtimezone.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gtrace.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gtranslit.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gtrashstack.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gtree.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gunibreak.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gunicollate.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gunidecomp.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/guniprop.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/guri.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gutf8.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gutils.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/guuid.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gvariant-core.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gvariant-parser.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gvariant-serialiser.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gvariant.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gvarianttype.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gvarianttypeinfo.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gversion.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gwakeup.c".}
+    {.compile: "chafa/glib/gthread-posix.c".}
+  {.compile: "chafa/glib/gthread.c".}
+  {.compile: "chafa/glib/gthreadpool.c".}
+  {.compile: "chafa/glib/gtimer.c".}
+  #{.compile: "chafa/glib/gtimezone.c".}
+  #{.compile: "chafa/glib/gtrace.c".}
+  {.compile: "chafa/glib/gtranslit.c".}
+  {.compile: "chafa/glib/gtrashstack.c".}
+  #{.compile: "chafa/glib/gtree.c".}
+  #{.compile: "chafa/glib/gunibreak.c".}
+  #{.compile: "chafa/glib/gunicollate.c".}
+  {.compile: "chafa/glib/gunidecomp.c".}
+  {.compile: "chafa/glib/guniprop.c".}
+  {.compile: "chafa/glib/guri.c".}
+  {.compile: "chafa/glib/gutf8.c".}
+  {.compile: "chafa/glib/gutils.c".}
+  #{.compile: "chafa/glib/guuid.c".}
+  {.compile: "chafa/glib/gvariant-core.c".}
+  {.compile: "chafa/glib/gvariant-parser.c".}
+  {.compile: "chafa/glib/gvariant-serialiser.c".}
+  {.compile: "chafa/glib/gvariant.c".}
+  {.compile: "chafa/glib/gvarianttype.c".}
+  {.compile: "chafa/glib/gvarianttypeinfo.c".}
+  #{.compile: "chafa/glib/gversion.c".}
+  {.compile: "chafa/glib/gwakeup.c".}
   when defined(windows):
-    #{.compile: "src/ansiwavepkg/chafa/glib/gwin32-private.c".}
-    {.compile: "src/ansiwavepkg/chafa/glib/gwin32.c".}
+    #{.compile: "chafa/glib/gwin32-private.c".}
+    {.compile: "chafa/glib/gwin32.c".}
   when defined(macosx):
-    {.compile: "src/ansiwavepkg/chafa/glib/gosxutils.m".}
+    {.compile: "chafa/glib/gosxutils.m".}
 
-  {.compile: "src/ansiwavepkg/chafa/glib/libcharset/localcharset.c".}
+  {.compile: "chafa/glib/libcharset/localcharset.c".}
 
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/asnprintf.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/frexp.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/frexpl.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gnulib/isinf.c".}
-  #{.compile: "src/ansiwavepkg/chafa/glib/gnulib/isnan.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/isnand.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/isnanf.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/isnanl.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/printf-args.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/printf-frexp.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/printf-frexpl.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/printf-parse.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/printf.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/signbitd.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/signbitf.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/signbitl.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/vasnprintf.c".}
-  {.compile: "src/ansiwavepkg/chafa/glib/gnulib/xsize.c".}
+  {.compile: "chafa/glib/gnulib/asnprintf.c".}
+  {.compile: "chafa/glib/gnulib/frexp.c".}
+  {.compile: "chafa/glib/gnulib/frexpl.c".}
+  #{.compile: "chafa/glib/gnulib/isinf.c".}
+  #{.compile: "chafa/glib/gnulib/isnan.c".}
+  {.compile: "chafa/glib/gnulib/isnand.c".}
+  {.compile: "chafa/glib/gnulib/isnanf.c".}
+  {.compile: "chafa/glib/gnulib/isnanl.c".}
+  {.compile: "chafa/glib/gnulib/printf-args.c".}
+  {.compile: "chafa/glib/gnulib/printf-frexp.c".}
+  {.compile: "chafa/glib/gnulib/printf-frexpl.c".}
+  {.compile: "chafa/glib/gnulib/printf-parse.c".}
+  {.compile: "chafa/glib/gnulib/printf.c".}
+  {.compile: "chafa/glib/gnulib/signbitd.c".}
+  {.compile: "chafa/glib/gnulib/signbitf.c".}
+  {.compile: "chafa/glib/gnulib/signbitl.c".}
+  {.compile: "chafa/glib/gnulib/vasnprintf.c".}
+  {.compile: "chafa/glib/gnulib/xsize.c".}
 
   type
     GString* {.bycopy.} = object
