c81118053b0743e242c4e1b0cd83e986fe229949
oakes <zach@sekao.net>
2021-10-23 14:04:57 +0200

    Start work on ui system based on JsonNode

--- src/ansiwavepkg/bbs.nim
+++ src/ansiwavepkg/bbs.nim
@@ -1,11 +1,14 @@
 from illwill as iw import `[]`, `[]=`
 from wavecorepkg/db/vfs import nil
+from wavecorepkg/db/entities import nil
 from wavecorepkg/client import nil
 from os import joinPath
 from strutils import format
 import ./constants
 import unicode
 from ./codes import nil
+import json
+import tables
 
 const
   port = 3000
@@ -15,42 +18,100 @@ const
   staticFileDir = "tests".joinPath("bbs")
   dbPath = staticFileDir.joinPath(dbFilename)
 
-proc renderBBS*(tb: var iw.TerminalBuffer, root: var auto, threads: var auto) =
-  var screenLine = 0
-  client.get(root)
-  if root.ready:
-    let lines = strutils.splitLines(root.value.valid.body)
-    iw.drawRect(tb, 0, 0, editorWidth, lines.len + 1, doubleStyle = true)
-    for line in lines:
-      var runes = line.toRunes
-      codes.deleteAfter(runes, editorWidth - 1)
-      codes.write(tb, 1, 1 + screenLine, $runes)
-      screenLine += 1
-  screenLine += 2
-  client.get(threads)
-  if threads.ready:
-    for post in threads.value.valid:
-      let lines = strutils.splitLines(post.body.uncompressed)
-      iw.drawRect(tb, 0, screenLine, editorWidth, 1 + screenLine + lines.len, doubleStyle = false)
-      screenLine += 1
-      for line in lines:
-        iw.write(tb, 1, screenLine, line)
-        screenLine += 2
+type
+  Post = object
+    main: client.ChannelValue[client.Response]
+    replies: client.ChannelValue[seq[entities.Post]]
+
+proc init*[Post](c: client.Client, id: int): Post =
+  result.main = client.query(c, ansiwavesDir.joinPath($id & ".ansiwavez"))
+  result.replies = client.queryPostChildren(c, dbFilename, id)
+
+proc render*(tb: var iw.TerminalBuffer, node: JsonNode, x: int, y: var int)
+
+proc render*(post: entities.Post): JsonNode =
+  %*{
+    "type": "rect",
+    "children": strutils.splitLines(post.body.uncompressed)
+  }
+
+proc render*(posts: seq[entities.Post]): JsonNode =
+  result = JsonNode(kind: JArray)
+  for post in posts:
+    result.elems.add(render(post))
+
+proc render*(comp: var Post): JsonNode =
+  client.get(comp.main)
+  client.get(comp.replies)
+  %*[
+    if not comp.main.ready:
+      %"Loading..."
+    elif comp.main.value.kind == client.Error:
+      %"Failed to load!"
+    else:
+      %*{
+        "type": "rect",
+        "children": strutils.splitLines(comp.main.value.valid.body)
+      }
+    ,
+    "", # spacer
+    if not comp.replies.ready:
+      %"Loading replies"
+    elif comp.replies.value.kind == client.Error:
+      %"Failed to load replies!"
+    else:
+      if comp.replies.value.valid.len == 0:
+        %"No replies"
+      else:
+        render(comp.replies.value.valid)
+  ]
+
+proc render*(tb: var iw.TerminalBuffer, node: string, x: int, y: var int) =
+  var runes = node.toRunes
+  codes.deleteAfter(runes, editorWidth - 1)
+  y += 1
+  codes.write(tb, x, y, $runes)
+
+proc render*(tb: var iw.TerminalBuffer, node: OrderedTable[string, JsonNode], x: int, y: var int) =
+  case node["type"].str:
+  of "rect":
+    let
+      xStart = x
+      yStart = y
+    for child in node["children"]:
+      render(tb, child, x + 1, y)
+    y += 1
+    iw.drawRect(tb, xStart, yStart, editorWidth, y, doubleStyle = false)
+    y += 1
+
+proc render*(tb: var iw.TerminalBuffer, node: JsonNode, x: int, y: var int) =
+  case node.kind:
+  of JString:
+    render(tb, node.str, x, y)
+  of JObject:
+    render(tb, node.fields, x, y)
+  of JArray:
+    for item in node.elems:
+      render(tb, item, x, y)
+  else:
+    raise newException(Exception, "Unhandled JSON type")
+
+proc render*(tb: var iw.TerminalBuffer, post: var Post) =
+  var y = 0
+  render(tb, render(post), 0, y)
 
 proc renderBBS*() =
   vfs.readUrl = "http://localhost:" & $port & "/" & dbFilename
   vfs.register()
   var c = client.initClient(address)
   client.start(c)
-  var
-    root = client.query(c, ansiwavesDir.joinPath("1.ansiwavez"))
-    threads = client.queryPostChildren(c, dbFilename, 1)
+  var post = init[Post](c, 1)
   while true:
     let
       width = iw.terminalWidth()
       height = iw.terminalHeight()
     var tb = iw.newTerminalBuffer(width, height)
-    renderBBS(tb, root, threads)
+    render(tb, post)
     # display and sleep
     iw.display(tb)
     os.sleep(sleepMsecs)
