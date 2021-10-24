091a0615eefbd8394fcfa23bb89e57101b671a94
oakes <zach@sekao.net>
2021-10-24 09:35:01 +0200

    Refactor

--- src/ansiwavepkg/bbs.nim
+++ src/ansiwavepkg/bbs.nim
@@ -17,8 +17,10 @@ type
   Attr* = enum
     SelectedColumn,
     ComponentData, FocusIndex,
-    ScrollY, View, ViewHeight,
+    ScrollY,
+    View, ViewHeight, ViewFocusAreas,
   ComponentRef = ref ui.Component
+  ViewFocusAreasType = seq[tuple[top: int, bottom: int]]
 
 schema Fact(Id, Attr):
   SelectedColumn: int
@@ -27,6 +29,7 @@ schema Fact(Id, Attr):
   ScrollY: int
   View: JsonNode
   ViewHeight: int
+  ViewFocusAreas: ViewFocusAreasType
 
 let rules =
   ruleset:
@@ -41,6 +44,7 @@ let rules =
         (id, ScrollY, scrollY)
         (id, View, view)
         (id, ViewHeight, viewHeight)
+        (id, ViewFocusAreas, viewFocusAreas)
 
 proc insert(session: var auto, comp: ui.Component) =
   let col = session.query(rules.getGlobals).selectedColumn
@@ -52,6 +56,7 @@ proc insert(session: var auto, comp: ui.Component) =
   session.insert(col, ScrollY, 0)
   session.insert(col, View, cast[JsonNode](nil))
   session.insert(col, ViewHeight, 0)
+  session.insert(col, ViewFocusAreas, @[])
 
 proc initSession*(c: client.Client): auto =
   result = initSession(Fact, autoFire = false)
@@ -65,7 +70,18 @@ proc render*(session: var auto, width: int, height: int, key: iw.Key, finishedLo
     comp = session.query(rules.getSelectedColumn)
     bufferHeight = max(comp.viewHeight, iw.terminalHeight())
     maxScroll = max(1, int(height / 5))
-    renderedFocusIndex =
+    view =
+      if comp.view != nil:
+        finishedLoading = true
+        comp.view
+      else:
+        let v = ui.toJson(comp.data[], finishedLoading)
+        if finishedLoading:
+          session.insert(comp.id, View, v)
+        v
+  result = iw.newTerminalBuffer(width, bufferHeight)
+  var
+    focusIndex =
       case key:
       of iw.Key.Up:
         if comp.focusIndex > 0:
@@ -76,61 +92,54 @@ proc render*(session: var auto, width: int, height: int, key: iw.Key, finishedLo
         comp.focusIndex + 1
       else:
         comp.focusIndex
-  result = iw.newTerminalBuffer(width, bufferHeight)
+    scrollY = comp.scrollY
+  # adjust focusIndex and scrollY based on viewFocusAreas
+  if comp.viewFocusAreas.len > 0:
+    # don't let it go beyond the last focused area
+    if focusIndex > comp.viewFocusAreas.len - 1:
+      focusIndex = comp.viewFocusAreas.len - 1
+    # when going up or down, if the next focus area's edge is
+    # beyond the current viewable scroll area, adjust scrollY
+    # so we can see it. if the adjustment is greater than maxScroll,
+    # only scroll maxScroll rows and update the focusIndex.
+    case key:
+    of iw.Key.Up:
+      if comp.viewFocusAreas[focusIndex].top < comp.scrollY:
+        scrollY = comp.viewFocusAreas[focusIndex].top
+        let limit = comp.scrollY - maxScroll
+        if scrollY < limit:
+          scrollY = limit
+          for i in 0 .. comp.viewFocusAreas.len - 1:
+            if comp.viewFocusAreas[i].bottom > limit:
+              focusIndex = i
+              break
+    of iw.Key.Down:
+      if comp.viewFocusAreas[focusIndex].bottom > comp.scrollY + height:
+        scrollY = comp.viewFocusAreas[focusIndex].bottom - height
+        let limit = comp.scrollY + maxScroll
+        if scrollY > limit:
+          scrollY = limit
+          for i in countdown(comp.viewFocusAreas.len - 1, 0):
+            if comp.viewFocusAreas[i].top < limit + height:
+              focusIndex = i
+              break
+    else:
+      discard
+  # update values if necessary
+  if focusIndex != comp.focusIndex:
+    session.insert(comp.id, FocusIndex, focusIndex)
+  if scrollY != comp.scrollY:
+    session.insert(comp.id, ScrollY, scrollY)
+  # render
   var
     y = 0
     blocks: seq[tuple[top: int, bottom: int]]
-  let view =
-    if comp.view != nil:
-      finishedLoading = true
-      comp.view
-    else:
-      let v = ui.toJson(comp.data[], finishedLoading)
-      if finishedLoading:
-        session.insert(comp.id, View, v)
-      v
-  ui.render(result, view, 0, y, key, comp.scrollY, renderedFocusIndex, blocks)
+  ui.render(result, view, 0, y, key, scrollY, focusIndex, blocks)
   # update the view height if it has increased
   if blocks.len > 0 and blocks[blocks.len - 1].bottom > comp.viewHeight:
     session.insert(comp.id, ViewHeight, blocks[blocks.len - 1].bottom)
-  var focusIndex = renderedFocusIndex
-  # adjust scroll and reset focusIndex if necessary
-  if blocks.len > 0:
-    if focusIndex > blocks.len - 1:
-      focusIndex = blocks.len - 1
-    case key:
-    of iw.Key.Up:
-      if blocks[focusIndex].top < comp.scrollY:
-        let limit = comp.scrollY - maxScroll
-        if blocks[focusIndex].top < limit:
-          session.insert(comp.id, ScrollY, limit)
-          for i in 0 .. blocks.len - 1:
-            if blocks[i].bottom > limit:
-              focusIndex = i
-              break
-        else:
-          session.insert(comp.id, ScrollY, blocks[focusIndex].top)
-    of iw.Key.Down:
-      if blocks[focusIndex].bottom > comp.scrollY + height:
-        let limit = comp.scrollY + maxScroll
-        if blocks[focusIndex].bottom - height > limit:
-          session.insert(comp.id, ScrollY, limit)
-          for i in countdown(blocks.len - 1, 0):
-            if blocks[i].top < limit + height:
-              focusIndex = i
-              break
-        else:
-          session.insert(comp.id, ScrollY, blocks[focusIndex].bottom - height)
-    else:
-      discard
-  if focusIndex != comp.focusIndex:
-    session.insert(comp.id, FocusIndex, focusIndex)
-  # if focusIndex was reset, re-render so the correct block has the double lines
-  if focusIndex != renderedFocusIndex:
-    y = 0
-    blocks = @[]
-    ui.render(result, view, 0, y, key, comp.scrollY, focusIndex, blocks)
-  let scrollY = session.query(rules.getSelectedColumn).scrollY
+    session.insert(comp.id, ViewFocusAreas, blocks)
+  # adjust buffer so part above the scroll line isn't visible
   if (scrollY + height) <= bufferHeight:
     result.height = height
     result.buf = result.buf[scrollY * width ..< result.buf.len]
