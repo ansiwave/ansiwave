c70be53f9e0d9287997d414fe09e1c1a50f52322
oakes <zach@sekao.net>
2021-11-08 18:11:40 +0100

    Make search field a button

--- src/ansiwavepkg/ui/navbar.nim
+++ src/ansiwavepkg/ui/navbar.nim
@@ -22,37 +22,12 @@ proc renderButton(tb: var iw.TerminalBuffer, text: string, x: int, y: int, key: 
     cb()
   result += 1
 
-proc renderTextField(tb: var iw.TerminalBuffer, text: string, x: int, y: int, width: int, key: iw.Key, cb: proc (), shortcut: tuple[key: set[iw.Key], hint: string] = ({}, "")) =
-  let
-    endX = x + width
-    endY = y + height - 1
-  iw.drawRect(tb, x, y, endX, endY, doubleStyle = false)
-  iw.write(tb, x + 1, y + 1, text)
-  if key == iw.Key.Mouse:
-    let info = iw.getMouse()
-    if info.button == iw.MouseButton.mbLeft and info.action == iw.MouseButtonAction.mbaPressed:
-      if info.x >= x and
-          info.x < endX and
-          info.y >= y and
-          info.y < endY:
-        cb()
-  elif key in shortcut.key:
-    cb()
-
 proc render*(tb: var iw.TerminalBuffer, pageX: int, pageY: int, input: tuple[key: iw.Key, codepoint: uint32], backAction: proc(), rightButtonText: string, rightButtonAction: proc (), showSearch: bool = true) =
   iw.fill(tb, pageX, pageY, pageX + constants.editorWidth + 1, pageY + height - 1)
   var x = pageX
   x = renderButton(tb, " ← ", x, pageY, input.key, backAction)
-  if rightButtonText != "":
-    discard renderButton(tb, rightButtonText, constants.editorWidth - rightButtonText.runeLen, pageY, input.key, rightButtonAction)
-  let rightButtonWidth =
-    if rightButtonText != "":
-      rightButtonText.runeLen + 2
-    else:
-      0
   if showSearch:
-    let
-      searchWidth = constants.editorWidth - x - rightButtonWidth + 1
-      searchText = " Press / to search "
-    renderTextField(tb, searchText, x, pageY, searchWidth, input.key, proc () = discard)
+    x = renderButton(tb, " / Search ", x, pageY, input.key, proc () = discard)
+  if rightButtonText != "":
+    discard renderButton(tb, rightButtonText, max(x, constants.editorWidth - rightButtonText.runeLen), pageY, input.key, rightButtonAction)
 
