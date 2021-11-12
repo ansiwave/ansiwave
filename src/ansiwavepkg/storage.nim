951e1aef3fee434a2c22c122f2ad16158951af7e
oakes <zach@sekao.net>
2021-11-12 16:52:04 +0100

    Use base58 when saving to local storage

--- src/ansiwavepkg/storage.nim
+++ src/ansiwavepkg/storage.nim
@@ -1,6 +1,6 @@
 when defined(emscripten):
   from wavecorepkg/client/emscripten import nil
-  from base64 import nil
+  from wavecorepkg/base58 import nil
 else:
   from os import `/`
 
@@ -10,7 +10,7 @@ proc set*(key: string, val: string | seq[uint8], isBinary: bool = false): bool =
   when defined(emscripten):
     let v =
       if isBinary:
-        base64.encode(val)
+        base58.encode(val)
       else:
         cast[string](val)
     emscripten.localSet(key, v)
@@ -27,7 +27,7 @@ proc get*(key: string, isBinary: bool = false): string =
   when defined(emscripten):
     let val = emscripten.localGet(key)
     if isBinary:
-      base64.decode(val)
+      base58.decode(val)
     else:
       val
   else:
