11cf0b061a6a5ecdcdd61ab8002bd4dbb25478d4
oakes <zach@sekao.net>
2021-11-30 22:20:47 +0100

    Update text

--- src/ansiwavepkg/ui.nim
+++ src/ansiwavepkg/ui.nim
@@ -152,7 +152,7 @@ proc toJson*(draft: Draft): JsonNode =
     },
     {
       "type": "button",
-      "text": if isNew: "post that this is replying to" else: "post that this is editing",
+      "text": if isNew: "see post that this is replying to" else: "see post that this is editing",
       "action": "show-post",
       "action-data": {"type": "post", "sig": originalSig},
     },
