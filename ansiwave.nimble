8c2283afc756460d4ad4104ea639923f56ad9cad
oakes <zach@sekao.net>
2022-01-23 09:02:01 +0100

    Bump version

--- ansiwave.nimble
+++ ansiwave.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "1.3.5"
+version       = "1.4.0"
 author        = "oakes"
 description   = "ANSI art + MIDI music"
 license       = "Public Domain"
--- src/ansiwave.nim
+++ src/ansiwave.nim
@@ -23,7 +23,7 @@ from ./ansiwavepkg/ui/editor import nil
 from terminal import nil
 from wavecorepkg/wavescript import CommandTree
 
-const version = "1.3.5"
+const version = "1.4.0"
 
 proc exitClean(ex: ref Exception) =
   if iw.gIllwillInitialised:
