d78b1287ebd70962c9666fdf5dd296c4cbc33260
oakes <zach@sekao.net>
2022-12-14 19:22:40 -0500

    Add cast

--- src/ansiwavepkg/chafa/glib/libcharset/localcharset.c
+++ src/ansiwavepkg/chafa/glib/libcharset/localcharset.c
@@ -117,7 +117,7 @@ _g_locale_get_charset_aliases (void)
       const char *base = "charset.alias";
       char *file_name;
 
-      dir = relocate (GLIB_CHARSETALIAS_DIR);
+      dir = (const char *) relocate (GLIB_CHARSETALIAS_DIR);
 
       /* Concatenate dir and base into freshly allocated file_name.  */
       {
